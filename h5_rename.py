b65fa41d9b97b51607c13c12a7bef4dea6f10b28
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-02-20 17:40:27 +0000

    Cleaned up comments

--- h5_rename.py
+++ h5_rename.py
@@ -1,24 +1,3 @@
-'''
-A script to a set of files pertaining to a specific experiment. The files are in the form of .h5 files and are named in the following format:
-    - '{prefix}.run'
-    - '{prefix}_1.nxs'
-    - '{prefix}_1_000001.h5'
-    - '{prefix}_1_header.cbf'
-    - '{prefix}_1_master.h5'
-    - '{prefix}_1_meta.h5'
-
-The script will rename the files to the following format:
-    - '{new prefix}.run'
-    - '{new prefix}_1.nxs'
-    - '{new prefix}_1_000001.h5'
-    - '{new prefix}_1_header.cbf'
-    - '{new prefix}_1_master.h5'
-    - '{new prefix}_1_meta.h5'
-
-The script will also update the internal metadata of the .h5 files to reflect the new prefix.
-The script will also update the .nxs and .run files to reflect the new prefix.
-'''
-
 from argparse import ArgumentParser
 import h5py
 import os
